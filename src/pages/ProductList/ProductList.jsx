63a0638f963d0a71115bba31248567cf4ecc6c5b
Abhishek Singh <singhabhishek111213@gmail.com>
2024-08-13 15:17:55 +0530

    Added product box card to product list page

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import "./ProductList.css";
+
+import ProductImage from "../../assets/product.jpg";
+import ProductBox from "../../components/ProductBox/ProductBox";
 
 function ProductList() {
     return (
-        <div>ProductList</div>
+        <div className="container">
+            <div className="row">
+                <h2 className="product-list-title text-center">All Products</h2>
+                <div className="product-list-wrapper d-flex flex-row">
+                    {/* List of products */}
+                    <div className="product-list-box" id="productList">
+                        <ProductBox productImage={ProductImage} name={"dummy"} price={1000} />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
