4beddeafa94b1c0204df71298335bb9625443dd2
Abhishek Singh <singhabhishek111213@gmail.com>
2024-08-13 13:05:36 +0530

    Added homepage UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import './App.css';
 
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Home from './pages/home/Home';
 
 function App() {
 
   return (
     <div className="app-wrapper">
-      <Header color="light" light={true} expand="md" container="md" />
-      <div>
-        Some Content
-      </div>
+      {/* Common Header for all Pages */}
+      <Header
+        color="light"
+        light={true}
+        expand="md"
+        container="md" />
+
+      <Home />
+
+      {/* Common Footer for all Pages */}
       <Footer />
     </div>
   );
